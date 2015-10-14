4fa6e5fc0b55d80154a23d1884c5bee1734a7281
WalterGoedecke <goedecke@txcorp.com>
2015-10-14 15:33:50 -0600

    Why doing this?

--- SolarFS2c/SolarFS2c/settings.py
+++ SolarFS2c/SolarFS2c/settings.py
@@ -199,6 +199,7 @@ STATICFILES_DIRS = (
     (os.path.join(PROJECT_ROOT, 'classic/static/classic/js/images/')),
     (os.path.join(PROJECT_ROOT, 'classic/static/classic_std/')),
     (os.path.join(PROJECT_ROOT, 'static/media/')),
+    (os.path.join(PROJECT_ROOT, 'static/media/uploads/')),
 )
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
