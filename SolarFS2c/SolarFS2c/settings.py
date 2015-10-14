149235b7d9aa5fb1ed258783418f1d7feb7b64d6
WalterGoedecke <goedecke@txcorp.com>
2015-10-14 16:47:11 -0600

    Adding 'SolarFS2c' directory from 'branch1' to master.

--- SolarFS2c/SolarFS2c/settings.py
+++ SolarFS2c/SolarFS2c/settings.py
@@ -199,7 +199,6 @@ STATICFILES_DIRS = (
     (os.path.join(PROJECT_ROOT, 'classic/static/classic/js/images/')),
     (os.path.join(PROJECT_ROOT, 'classic/static/classic_std/')),
     (os.path.join(PROJECT_ROOT, 'static/media/')),
-    (os.path.join(PROJECT_ROOT, 'static/media/uploads/')),
 )
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
