406b96f6cb24c8e156674f79ae5932201e44a8ee
WalterGoedecke <goedecke@txcorp.com>
2015-10-15 17:42:23 -0600

    Trying to merge branch1 SolarFS2c onto master

---
+++ SolarFS2c/classic/admin.py
@@ -0,0 +1,9 @@
+#from django.contrib import admin
+
+# Register your models here.
+#from mezzanine.pages.admin import PageAdmin
+#from .models import DocPage
+
+from classic.models import *
+
+admin.site.register(Calculation, CalculationAdmin)
